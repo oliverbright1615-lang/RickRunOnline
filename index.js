401a050df50dca0e5de4083d98cab9f0e671066a
oliverbright1615-lang <oliverbright1615@gmail.com>
2025-12-23 16:43:07 +0000

    index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const path = require('path'); // Add this at the very top
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-// Replace your old app.use line with this:
+const PORT = process.env.PORT || 10000;
+
+// This line tells Render to look inside the "public" folder for your game
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Add this "Safety Route" at the very bottom (above http.listen)
-app.get('*', (req, res) => {
+// This makes sure that if someone goes to your link, they see index.html
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A player joined!');
+});
+
+http.listen(PORT, '0.0.0.0', () => {
+  console.log('Server is running on port ' + PORT);
 });
